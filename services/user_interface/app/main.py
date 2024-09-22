b6a613a7ca0c6ab25fea0cbdabd7527f35a79b4f
msahmed1 <shabaj_ahmed@outlook.com>
2024-09-22 14:37:00 +0100

    Refactor UI code and add MQTT client

--- services/user_interface/app/main.py
+++ services/user_interface/app/main.py
@@ -25,17 +25,36 @@ class Color(QWidget):
         self.setPalette(palette)
 
 
+class Dashboard(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        vLayout = QVBoxLayout()
+
+        vLayout.addWidget(QLabel("Welcome to the app"))
+        vLayout.addWidget(QLabel("Implementation intention"))
+
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(QLabel("Check in Date and Time"))
+        hLayout.addWidget(QLabel("Days remaining"))
+
+        vLayout.addLayout(hLayout)
+        self.setLayout(vLayout)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("My App ")
 
         tabs = QTabWidget()
         tabs.setTabPosition(QTabWidget.TabPosition.West)
         tabs.setMovable(True)
 
-        tabs.addTab(Color("red"), "Home")
+        dashboard = Dashboard()
+
+        tabs.addTab(dashboard, "Home")
         tabs.addTab(Color("blue"), "Check-In")
         tabs.addTab(Color("green"), "History")
         tabs.addTab(Color("yellow"), "Profile")
