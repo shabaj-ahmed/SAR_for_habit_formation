4331bbb4b2eeedaf5518af2ecf737da5deaf5a8f
msahmed1 <shabaj_ahmed@outlook.com>
2024-11-11 21:46:56 +0000

    Refactor finite state machine states and add unit tests for state transitions

--- services/state_managment/app/deliberate_layer/finite_state_machine.py
+++ services/state_managment/app/deliberate_layer/finite_state_machine.py
@@ -9,69 +9,158 @@ class State:
         pass
 
 
+class OffState(State):
+    def __init__(self):
+        self.stateName = 'Off'
+
+    def enter(self):
+        # Turn everything off
+        pass
+
+    def exit(self):
+        pass
+
+    def update(self):
+        pass
+
+
 class SleepState(State):
+    def __init__(self):
+        self.stateName = 'Sleep'
+
     def enter(self):
-        print("Entering sleep state")
+        # Turn everything off
+            # Screen
+            # Robot
+            # Voice assistant
+            # Task scheduler
+        pass
 
     def exit(self):
-        print("Exiting sleep state")
+        pass
 
     def update(self):
         pass
 
 
-class AwakeState(State):
+class ActiveState:
+    def __init__(self):
+        self.stateName = 'Active'
+
     def enter(self):
-        print("Entering awake state")
-
-    def exit(self):
-        print("Exiting awake state")
-
-    def update(self):
+        # screen
+        # Task scheduler
         pass
 
-
-class IdleState:
-    def enter(self):
-        print("Entering idle state")
-
     def exit(self):
-        print("Exiting idle state")
+        pass
 
     def update(self):
         pass
 
 
 class InteractingState:
+    def __init__(self):
+        self.stateName = 'Interacting'
+
     def enter(self):
-        print("Entering interacting state")
+        # Turn on critical services
+            # Robot
+            # screen
+            # voice assistant
+            # Data collection
+        pass
 
     def exit(self):
-        print("Exiting interacting state")
+        # Robot
+        # voice assistant
+        pass
+
+    def update(self):
+        pass
+
+class ConfiguringState:
+    def __init__(self):
+        self.stateName = 'Configuring'
+
+    def enter(self):
+        # Turn on critical services
+            # Robot
+            # screen
+        pass
+
+    def exit(self):
+        pass
+
+    def update(self):
+        pass
+
+class ErrorState:
+    def __init__(self):
+        self.stateName = 'Error'
+
+    def enter(self):
+        pass
+
+    def exit(self):
+        pass
 
     def update(self):
         pass
 
 
 class FSM:
-    def __init__(self, initial_state):
+    def __init__(self, mqtt_client, initial_state, subsumption_layer_event_queue, finite_state_machine_event_queue, behavior_tree_event_queue):
+        self.mqtt_client = mqtt_client
+
+        self.subsumption_layer_event_queue = subsumption_layer_event_queue
+        self.finite_state_machine_event_queue = finite_state_machine_event_queue
+        self.behavior_tree_event_queue = behavior_tree_event_queue
+
         self.states = {
+            'Off': OffState(),
             'Sleep': SleepState(),
-            'Awake': AwakeState(),
-            'Idle': IdleState(),
+            'Active': ActiveState(),
             'Interacting': InteractingState(),
-            'Active': AwakeState(),
+            'Configuring': ConfiguringState(),
+            'Error': ErrorState()
         }
-        self.state = self.states[initial_state]
-        self.state.enter()
-
-    def update(self):
-        self.state.update()
-
+        self.currentState = self.states[initial_state]
+        self.currentState.enter()
+    
+    def get_state(self):
+        return self.currentState.stateName        
+    
     def transition_to(self, state_name):
         if state_name in self.states:
-            self.state.exit()
-            self.state = self.states[state_name]
-            self.state.enter()
+            self.currentState.exit()
+            self.currentState = self.states[state_name]
+            self.currentState.enter()
+            self.finite_state_machine_event_queue.put({"layer": "finite_state_machine", "state": state_name})
+            self.mqtt_client.publish("finite_state_machine", state_name)
         else:
             print(f"State {state_name} not found.")
+    
+    def update(self):
+        if not self.subsumption_layer_event_queue.empty():
+            subsumption_event = self.subsumption_layer_event_queue.get()
+            if subsumption_event is not None:
+                if subsumption_event["state"] == "Error":
+                    self.transition_to("Error")
+                elif subsumption_event["state"] == "Sleep":
+                    self.transition_to("Sleep")
+                elif subsumption_event["state"] == "Active":
+                    self.transition_to("Active")
+                elif subsumption_event["state"] == "Off":
+                    self.transition_to("Off")
+        if not self.behavior_tree_event_queue.empty():
+            behavior_tree_event = self.behavior_tree_event_queue.get()
+            if behavior_tree_event is not None:
+                if behavior_tree_event["state"] == "interacting":
+                    self.transition_to("Interacting")
+                elif behavior_tree_event["state"] == "configuring":
+                    self.transition_to("Configuring")
+        
+        # Calls update method of the current state
+        self.currentState.update()
+    
