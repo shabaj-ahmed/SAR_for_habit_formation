081631b93831a9b6e9ee1d7f3dadf4dd1b1a2c42
msahmed1 <shabaj_ahmed@outlook.com>
2024-11-14 19:17:38 +0000

    Updated MQTT client to manage user events and behavior completion status

--- services/state_managment/app/mqtt_client.py
+++ services/state_managment/app/mqtt_client.py
@@ -11,8 +11,16 @@ class MQTTClient:
 
         self.inputs = {
             'switch_state': None,
-            'check_in': None,
             'error': None
+        }
+        self.userEvents = {
+            'check_in': None,
+            'configurations': None
+        }
+        self.behaviourCompletionStatus = {
+            'reminder': False,
+            'check_in': False,
+            'configurations': False
         }
 
     def on_connect(self, client, userdata, flags, rc):
@@ -31,33 +39,81 @@ class MQTTClient:
         self.mqtt_client.subscribe("robot/switch_state")
         self.mqtt_client.message_callback_add(
             "robot/switch_state", self.process_switch_state)
+        
+        self.mqtt_client.subscribe("robot/error")
+        self.mqtt_client.message_callback_add(
+            "robot/error", self.process_error_message)
 
         self.mqtt_client.subscribe("robot/check_in")
         self.mqtt_client.message_callback_add(
             "robot/check_in", self.process_check_in)
-
-        self.mqtt_client.subscribe("robot/error")
+        
+        self.mqtt_client.subscribe("robot/configurations")
         self.mqtt_client.message_callback_add(
-            "robot/error", self.process_error_message)
+            "robot/configurations", self.process_configurations
+        )
+
+        self.mqtt_client.subscribe("robot/reminder")
+        self.mqtt_client.message_callback_add(
+            "robot/reminder", self.process_reminder_status
+        )
+
+        self.mqtt_client.subscribe("robot/check_in")
+        self.mqtt_client.message_callback_add(
+            "robot/check_in", self.process_check_in_status
+        )
+
 
     def process_switch_state(self, client, userdata, message):
         print("Processing switch state")
         self.inputs['switch_state'] = message.payload.decode()
 
-    def process_check_in(self, client, userdata, message):
-        print("Processing check in")
-        print("Check in message received")
-        if message.payload.decode() == '1':
-            self.inputs['check_in'] = True
-        else:
-            self.inputs['check_in'] = False
-
     def process_error_message(self, client, userdata, message):
         print("Processing error message")
         self.inputs['error'] = message.payload.decode()
 
+    def process_check_in(self, client, userdata, message):
+        print("Processing check in")
+        print("Check in message received")
+        if message.payload.decode() == '1':
+            self.userEvents['check_in'] = True
+        else:
+            self.userEvents['check_in'] = False
+            self.userEvents['configurations'] = False
+
+    def process_configurations(self, client, userdata, message):
+        print("Processing configurations")
+        print("Configurations message received")
+        if message.payload.decode() == '1':
+            self.userEvents['configurations'] = True
+        else:
+            self.userEvents['configurations'] = False
+            self.userEvents['check_in'] = False
+
+    def process_reminder_status(self, client, userdata, message):
+        print("Processing reminder status")
+        print("Reminder message received")
+        if message.payload.decode() == 'running':
+            self.behaviourCompletionStatus['reminder'] = False
+        elif message.payload.decode() == 'completed':
+            self.behaviourCompletionStatus['reminder'] = True
+    
+    def process_check_in_status(self, client, userdata, message):
+        print("Processing check in status")
+        print("Check in message received")
+        if message.payload.decode() == 'running':
+            self.behaviourCompletionStatus['check_in'] = False
+        elif message.payload.decode() == 'completed':
+            self.behaviourCompletionStatus['check_in'] = True
+
     def get_inputs(self):
         return self.inputs
+    
+    def get_user_event(self):
+        return self.userEvents
+    
+    def get_behaviour_completion_status(self):
+        return self.behaviourCompletionStatus
 
     def disconnect(self):
         self.mqtt_client.loop_stop()
