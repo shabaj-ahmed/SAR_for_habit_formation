146cb83927130e6087d558d88c5bdcfb1c9d6320
msahmed1 <shabaj_ahmed@outlook.com>
2024-11-24 21:18:17 +0000

    Update log file path in setup_logger function across services

--- services/user_interface/app/custom_logging/logging_config.py
+++ services/user_interface/app/custom_logging/logging_config.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-def setup_logger(log_file="../loggin/logs/app.log", level=logging.DEBUG):
+def setup_logger(log_file="custom_logging/logs/app.log", level=logging.DEBUG):
     # Ensure the directory for the log file exists
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
